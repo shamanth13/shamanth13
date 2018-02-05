27054c628b6036e3289f265d841652fb10f04705
shamanth13 <35687852+shamanth13@users.noreply.github.com>
2018-02-05 15:32:43 +0530

    Add files via upload

---
+++ addition 2.py
@@ -0,0 +1,9 @@
+#Addition of numbers using input command
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+c=a+b
+print(c)
+a=float(input("enter a vslue"))
+b=float(input("enter b value"))
+c=a+b
+print(c)
